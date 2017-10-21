e712f186f560e273357b32e47a077f6a3a484075
dxy159 <richard.ni.wchs@gmail.com>
2017-10-21 00:39:18 -0400

    initial commit

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/', function (req, res) {
 
 // for Facebook verification
 app.get('/webhook/', function (req, res) {
-	if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+	if (req.query['hub.verify_token'] === 'RICHARDNIDXY159') {
 		res.send(req.query['hub.challenge'])
 	}
 	res.send('Error, wrong token')
@@ -30,3 +30,18 @@ app.get('/webhook/', function (req, res) {
 app.listen(app.get('port'), function() {
 	console.log('running on port', app.get('port'))
 })
+
+app.post('/webhook/', function (req, res) {
+    let messaging_events = req.body.entry[0].messaging
+    for (let i = 0; i < messaging_events.length; i++) {
+	    let event = req.body.entry[0].messaging[i]
+	    let sender = event.sender.id
+	    if (event.message && event.message.text) {
+		    let text = event.message.text
+		    sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+	    }
+    }
+    res.sendStatus(200)
+})
+
+const token = "EAAB4mW4xXvMBALWM1wNbWDhkyoI5RWgZAYjgZBAsIZA1q8QUNzWJPUqrbwlDy6hpR4lompGeOnoJrUWMlvEYVDvZBeEnlLlZCJqx3Ur1208hXe19nd5lDK5C11jWtPCn6zCj8ZBrsFaMztlZAMft2olUg5xPcCGCKDSweY4EO9MHRN0TZBvvyqZB5"

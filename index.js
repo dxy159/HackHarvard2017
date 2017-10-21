206caa263edc8310cc94e554eec6965901975d0a
dxy159 <richard.ni.wchs@gmail.com>
2017-10-21 00:44:57 -0400

    commit

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/', function (req, res) {
 
 // for Facebook verification
 app.get('/webhook/', function (req, res) {
-	if (req.query['hub.verify_token'] === 'RICHARDNIDXY159') {
+	if (req.query['hub.verify_token'] === 'YILINISANENGLISHNAME') {
 		res.send(req.query['hub.challenge'])
 	}
 	res.send('Error, wrong token')

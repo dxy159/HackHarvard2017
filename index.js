11b30378274013df0f85fb780d2dd459dd5495a4
dxy159 <richard.ni.wchs@gmail.com>
2017-10-21 00:49:29 -0400

    should respond

--- index.js
+++ index.js
@@ -45,3 +45,22 @@ app.post('/webhook/', function (req, res) {
 })
 
 const token = "EAAB4mW4xXvMBALWM1wNbWDhkyoI5RWgZAYjgZBAsIZA1q8QUNzWJPUqrbwlDy6hpR4lompGeOnoJrUWMlvEYVDvZBeEnlLlZCJqx3Ur1208hXe19nd5lDK5C11jWtPCn6zCj8ZBrsFaMztlZAMft2olUg5xPcCGCKDSweY4EO9MHRN0TZBvvyqZB5"
+
+function sendTextMessage(sender, text) {
+    let messageData = { text:text }
+    request({
+	    url: 'https://graph.facebook.com/v2.6/me/messages',
+	    qs: {access_token:token},
+	    method: 'POST',
+		json: {
+		    recipient: {id:sender},
+			message: messageData,
+		}
+	}, function(error, response, body) {
+		if (error) {
+		    console.log('Error sending messages: ', error)
+		} else if (response.body.error) {
+		    console.log('Error: ', response.body.error)
+	    }
+    })
+}
